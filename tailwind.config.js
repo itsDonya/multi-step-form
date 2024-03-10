dfc8b978e816eda98774ed18898560726d9bf785
Donya <thisis2nya@gmail.com>
2024-03-10 11:20:08 +0330

    core: setup default colors in tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ export default {
   content: [],
   theme: {
     extend: {},
+    colors: {
+      "marine-blue": "#032a59",
+      "purplish-blue": "#473dff",
+      "pastel-blue": "#adbeff",
+      "light-blue": "#bfe2fd",
+      "strawberry-red": "#ed3548",
+      "cool-gray": "#9699ab",
+      "light-gray": "#d6d9e6",
+      magnolia: "#f0f6ff",
+      alabaster: "#fafbff",
+      white: "#ffffff",
+    },
   },
   plugins: [],
-}
-
+};
